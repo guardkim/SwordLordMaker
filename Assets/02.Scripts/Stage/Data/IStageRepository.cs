03a028424ea12308fd663f089e3f033cbb3ce5b9
GuardKim <78101768+guardkim@users.noreply.github.com>
2026-01-12 16:12:42 +0900

    delete scripts

--- Assets/02.Scripts/Stage/Data/IStageRepository.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-public interface IStageRepository
-{
-    List<StageStat> LoadAll();
-    StageStat GetByStageId(int stageId);
-    int GetMaxStageId();
-}
--- Assets/02.Scripts/Stage/Data/StageStat.cs
+++
@@ -1,8 +0,0 @@
-// Stage 스탯 데이터 (불변 Value Object)
-// BGDatabase StageStat 테이블과 매핑
-public record StageStat(
-    int StageId,        // 스테이지 번호 (1, 2, 3...)
-    string StageName,   // 스테이지 이름 (예: "1-1 해골의숲")
-    string EnemyStatId, // 스폰할 Enemy 타입 (EnemyStat.Id 참조)
-    int SpawnCount      // 스폰할 Enemy 수량
-);
